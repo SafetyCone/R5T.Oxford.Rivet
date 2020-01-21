dc0e09b522d5e5d925d2830f6c3aafbe7add7bb2
MinexAutomation <minex.automation@gmail.com>
2020-01-21 14:12:54 -0800

    Added RivetOrganizationProvider implementation of IOrganizationProvider service.

--- source/R5T.Oxford.Rivet/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.Oxford.Rivet
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.Oxford.Rivet/Code/Services/Implementations/RivetOrganizationProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using R5T.Ostrogothia;
+using R5T.Ostrogothia.Rivet;
+
+
+namespace R5T.Oxford.Rivet
+{
+    public class RivetOrganizationProvider : IOrganizationProvider
+    {
+        public IOrganization GetOrganization()
+        {
+            var organization = RivetOrganization.Instance.Value;
+            return organization;
+        }
+    }
+}
