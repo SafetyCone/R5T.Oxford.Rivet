139feae94d8ae42d0bca4a7df1102325825a4867
MinexAutomation <minex.automation@gmail.com>
2020-01-30 15:04:57 -0800

    Added IServiceCollection extensions.

---
+++ source/R5T.Oxford.Rivet/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Dacia;
+
+
+namespace R5T.Oxford.Rivet
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the <see cref="RivetOrganizationProvider"/> implementation of <see cref="IOrganizationProvider"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceCollection AddRivetOrganizationProvider(this IServiceCollection services)
+        {
+            services.AddSingleton<IOrganizationProvider, RivetOrganizationProvider>();
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="RivetOrganizationProvider"/> implementation of <see cref="IOrganizationProvider"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static ServiceAction<IOrganizationProvider> AddRivetOrganizationProviderAction(this IServiceCollection services)
+        {
+            var serviceAction = new ServiceAction<IOrganizationProvider>(() => services.AddRivetOrganizationProvider());
+            return serviceAction;
+        }
+    }
+}
