f6d1fcae161e78e15068e0b4de616303c2faa3c4
davidcoats <david@getsafetycone.com>
2023-02-10 17:49:46 -0800

    Interim changes.

--- source/R5T.Oxford.Rivet/Code/Services/Implementations/RivetOrganizationProvider.cs
+++ source/R5T.Oxford.Rivet/Code/Services/Implementations/RivetOrganizationProvider.cs
@@ -1,16 +1,19 @@
 using System;
 
 using R5T.Ostrogothia;
-using R5T.Ostrogothia.Rivet;using R5T.T0064;
+using R5T.Ostrogothia.Rivet;
+
+using R5T.T0064;
 
 
 namespace R5T.Oxford.Rivet
-{[ServiceImplementationMarker]
-    public class RivetOrganizationProvider : IOrganizationProvider,IServiceImplementation
+{
+    [ServiceImplementationMarker]
+    public class RivetOrganizationProvider : IOrganizationProvider, IServiceImplementation
     {
         public IOrganization GetOrganization()
         {
-            var organization = RivetOrganization.Instance.Value;
+            var organization = RivetOrganization.Instance;
             return organization;
         }
     }
